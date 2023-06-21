3ba34d57e0de7ef14e828bb5b5627fc5fd8a054d
NullpointerW <825708370@qq.com>
2023-06-21 10:27:10 +0800

    fix flag parse

--- conf/flag.go
+++ conf/flag.go
@@ -15,10 +15,10 @@ var (
 func flaginit() {
 	flag.StringVar(&EnvPath, "e", "./env.yaml", "env yaml filepath")
 	flag.BoolVar(&debug, "d", false, "debug mode")
+	testing.Init()
+	flag.Parse()
 	if debug {
 		util.DebugEnv()
 	}
 	loginit(debug)
-	testing.Init()
-	flag.Parse()
 }
